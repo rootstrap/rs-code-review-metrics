a6eeb34a15110c3c64503cb3509a333bb9f3ef47
hdamico <hernan.damico@rootstrap.com>
2020-01-22 13:13:53 -0300

    Minor change

--- db/migrate/20200122143238_add_status_type_and_column.rb
+++ db/migrate/20200122143238_add_status_type_and_column.rb
@@ -1,4 +1,4 @@
-class AddTypeStatus < ActiveRecord::Migration[6.0]
+class AddStatusTypeAndColumn < ActiveRecord::Migration[6.0]
   def change
     execute <<-SQL
       CREATE TYPE status AS ENUM ('active', 'removed');
