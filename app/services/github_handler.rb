c41a1393833f0df920c58842590a23a226b4a331
Hernan D'Amico <hernan.damico67@gmail.com>
2020-01-23 10:47:11 -0300

    Comment change

--- app/services/github_handler.rb
+++ app/services/github_handler.rb
@@ -59,8 +59,6 @@ class GithubHandler
       merged: pr_data.merged,
       github_id: pr_data.id
     )
-  rescue ActiveRecord::RecordNotUnique
-    Rails.logger.error('This pull request already exists!')
   end
 
   def closed
