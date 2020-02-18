84ce5a40fd3ce4bed9aa66064d767417517859eb
Martin Rubi <martin.rubi@rootstrap.com>
2020-02-18 11:51:24 -0300

    Add schema annotations using the annotate gem

--- spec/factories/metrics.rb
+++ spec/factories/metrics.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: metrics
+#
+#  id              :bigint           not null, primary key
+#  entity_key      :string           not null
+#  metric_key      :string           not null
+#  value           :decimal(, )
+#  value_timestamp :datetime
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 FactoryBot.define do
   factory :metric do
     entity_key { 'generis' }
