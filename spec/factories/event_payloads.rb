1c1285c8b7a979b057a14887a015b43f52820f23
Martin Rubi <martin.rubi@rootstrap.com>
2020-02-19 12:16:07 -0300

    Implement :pull_request_review_payload factory
    Implement a factory with the common fields of the Github webhook pull_request_review payload.
    As there is an independent factory for Models, when using Model and Payload factories make sure that if the ids or other values
    must be in sync between Models and payloads they are built in sync.

---
+++ spec/factories/event_payloads.rb
@@ -0,0 +1,107 @@
+FactoryBot.define do
+  factory :user_payload, class: Hash do
+    id      { 1 }
+    node_id { 'MDQ6NlcjE4' }
+    login   { 'heptacat' }
+    type    { 'User' }
+
+    initialize_with do
+      {
+          id: id,
+          node_id: node_id,
+          login: login,
+          type: type
+        }.deep_stringify_keys
+    end
+  end
+
+  factory :repository_payload, class: Hash do
+    id        { 186853002 }
+    node_id   { 'MDEwOlJlcG9zaXRvcnkxODY4NTMwMDI=' }
+    name      { 'Pull Hello-World' }
+    full_name { 'Codertocat/Hello-World' }
+
+    association :pull_request_owner, factory: :user_payload,
+      id: 21031067, node_id: 'MDQ6VXNlcjIxMDMxMDY3', login: 'Codertocat'
+
+    initialize_with do
+      {
+          id: id,
+          node_id: node_id,
+          name: name,
+          full_name: full_name,
+          owner: pull_request_owner
+        }.deep_stringify_keys
+    end
+  end
+
+  factory :pull_request_payload, class: Hash do
+    id      { 1001 }
+    number  { 2 }
+    state   { 'open' }
+    node_id { 'MDExOlB1bGxSZXF1ZXN0Mjc5MTQ3NDM3' }
+    title   { 'Pull Request 2' }
+    locked  { false }
+    merged  { false }
+    draft   { false }
+
+    association :pull_request_owner, factory: :user_payload,
+      id: 21031067, node_id: 'MDQ6VXNlcjIxMDMxMDY3', login: 'Codertocat'
+
+    association :repository, factory: :repository_payload
+
+    initialize_with do
+      {
+          id: id,
+          number: number,
+          state: state,
+          node_id: node_id,
+          title: title,
+          locked: locked,
+          merged: merged,
+          draft: draft,
+          user: pull_request_owner,
+          repository: repository
+        }.deep_stringify_keys
+    end
+  end
+
+  factory :pull_request_review_payload, class: Hash do
+    action { 'submitted' }
+    id { 237895671 }
+    node_id { 'MDE3OlB1bGxSZXF1ZXN0UmV2aWV3MjM3ODk1Njcx' }
+    state { 'commented' }
+
+    association :review_owner, factory: :user_payload,
+      id: 21031067, node_id: 'MDQ6VXNlcjIxMDMxMDY3', login: 'Codertocat'
+
+    association :pull_request, factory: :pull_request_payload
+
+    association :repository, factory: :repository_payload
+
+    trait :submitted do
+        action { 'submitted' }
+    end
+    trait :edited do
+        action { 'edited' }
+    end
+    trait :dismissed do
+        action { 'dismissed' }
+    end
+
+    initialize_with do
+      {
+        action: action,
+        state: state,
+        review: {
+          id: id,
+          node_id: node_id,
+          user: review_owner
+        },
+        pull_request: pull_request,
+        repository: repository,
+        requested_reviewer: requested_reviewer
+      }.deep_stringify_keys
+    end
+  end
+end
