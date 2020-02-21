a2acf76d57ca7e1b3fe9db16574d296719dad154
Martin Rubi <martin.rubi@rootstrap.com>
2020-02-21 18:08:56 -0300

    Fix comment

--- app/services/metrics/metric.rb
+++ app/services/metrics/metric.rb
@@ -7,14 +7,14 @@ module Metrics
     end
 
     ##
-    # Processes the given events to generate the review turnaround metrics
+    # Processes the given events to generate the metrics.
     def call()
       process_events()
     end
 
 private
     ##
-    # Creates or updates the value of the metric with the key (entity_key, metric_key)
+    # Creates or updates the value of the metric with the key (entity_key, metric_key).
     def update_metric(entity_key:, metric_key:, value:, value_timestamp:)
       find_or_create_metric(entity_key: entity_key, metric_key: metric_key) do |metric|
         metric.value = value
