072ce251d8215c92fc959549619795b2b6c485d4
Martin Rubi <martin.rubi@rootstrap.com>
2020-02-21 13:30:23 -0300

    Refactor calls to Rake tasks to use more descriptive method names.

--- spec/lib/metrics_process_spec.rb
+++ spec/lib/metrics_process_spec.rb
@@ -3,13 +3,21 @@ require 'rake'
 
 RSpec.describe 'running the rake task metrics:process' do
   before do
+    load_rake_metrics
+  end
+
+  let(:load_rake_metrics) do
     Rake.application.rake_require 'tasks/metrics'
     Rake::Task.define_task(:environment)
+  end
+
+  let(:rake_process_metrics) do
+    Rake.application.invoke_task 'metrics:process'
   end
 
   it 'creates a ProcessMetricsJob and invokes it' do
       expect_any_instance_of(ProcessMetricsJob).to receive(:perform).once
 
-      Rake.application.invoke_task 'metrics:process'
+      rake_process_metrics
   end
 end
