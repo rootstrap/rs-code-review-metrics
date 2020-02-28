dff1e1bbf1436616b2b2a7bfebdb121c42c53eba
Martin Rubi <martin.rubi@rootstrap.com>
2020-02-28 15:42:59 -0300

    Removed redundant comments from the specs since it's more clear to read the test

--- spec/services/metrics/project_review_turnaround_processor_spec.rb
+++ spec/services/metrics/project_review_turnaround_processor_spec.rb
@@ -20,9 +20,6 @@ RSpec.describe Metrics::ReviewTurnaroundProcessor, type: :job do
   end
 
   describe 'when processing a collection containing no review_events' do
-    #  event :project, id: 'Project A', action: :created, at: '2019-01-01 15:10:00'
-    #  event :pull_request, id: 1, project: 'Project A', action: :opened, at: '2020-01-01 15:10:00'
-    #  event :anything_but :review, pull_request: 1
     let(:create_test_events) do
       pull_request_event_payload = create_pull_request_event(
         action: 'opened',
@@ -32,7 +29,6 @@ RSpec.describe Metrics::ReviewTurnaroundProcessor, type: :job do
       create_review_comment_event pull_request_event_payload: pull_request_event_payload
     end
 
-    #  expect metrics count: 0
     it 'does not create a metric' do
       expect(generated_metrics_count).to eq(0)
     end
@@ -40,28 +36,17 @@ RSpec.describe Metrics::ReviewTurnaroundProcessor, type: :job do
 
   context 'for a project' do
     describe 'with a PR that had not been reviewed yet' do
-      #  event :project, id: 10, action: :created, at: '2019-01-01 15:10:00'
-      #
-      # Partial metric:
-      #  event :pull_request, id: 1, project: 'Project A',
-      #   action: :opened, at: '2020-01-01 15:10:00'
       let(:create_test_events) do
-        create_pull_request_event action: 'opened',
-                                  created_at: Time.zone.parse('2020-01-01T15:10:00')
+        create_pull_request_event(action: 'opened',
+                                  created_at: Time.zone.parse('2020-01-01T15:10:00'))
       end
 
-      # expect metrics_for project id: 'Project A', count: 0
       it 'does not generate a metric' do
         expect(generated_metrics_count).to eq(0)
       end
     end
 
     describe 'with no previous review_turnaround metric in the given time interval' do
-      #  event :project, id: 10, action: :created, at: '2019-01-01 15:10:00'
-      #  event :pull_request, id: 1, project: 'Project A',
-      #   action: :opened, at: '2020-01-01 15:10:00'
-      #  event :review, pull_request: 1, action: :submitted,
-      #   at: '2020-01-01 15:30:00'
       let(:create_test_events) do
         pull_request_event_payload = create_pull_request_event(
           action: 'opened',
@@ -73,7 +58,6 @@ RSpec.describe Metrics::ReviewTurnaroundProcessor, type: :job do
                             submitted_at: Time.zone.parse('2020-01-01T15:30:00')
       end
 
-      # expect metrics_for project id: 'Project A', value: 20.minutes
       it 'generates a review_turnaround metric for the given interval' do
         expect(first_metric).to have_attributes(
           entity_key: 'Project A',
@@ -84,31 +68,12 @@ RSpec.describe Metrics::ReviewTurnaroundProcessor, type: :job do
         expect(first_metric_value_expressed_as_seconds).to be_within(1.second) .of(20.minutes)
       end
 
-      #  event :project, id: 10, action: :created, at: '2019-01-01 15:10:00'
-      #  event :pull_request, id: 1, project: 'Project A', action: :opened,
-      #   at: '2020-01-01 15:10:00'
-      #  event :review, pull_request_id: 1, action: :submitted,
-      #   at: '2020-01-01 15:30:00'
       it 'generates only that metric' do
-        # expect metrics count: 1
         expect(generated_metrics_count).to eq(1)
       end
     end
 
     describe 'with a previous review_turnaround metric in the given time interval' do
-      #  event :project, id: 10, action: :created, at: '2019-01-01 15:10:00'
-      #
-      # Partial metric:
-      #  event :pull_request, id: 1, project: 'Project A',
-      #   action: :opened, at: '2020-01-01 15:10:00'
-      #  event :review, pull_request: 1, action: :submitted,
-      #   at: '2020-01-01 15:30:00'
-      #
-      # Further events:
-      #  event :pull_request, id: 1, project: 'Project A',
-      #   action: :opened, at: '2020-01-01 16:00:00'
-      #  event :review, pull_request: 1, action: :submitted,
-      #   at: '2020-01-01 16:45:00'
       let(:create_test_events) do
         pull_request_event_payload = create_pull_request_event(
           action: 'opened',
@@ -134,7 +99,6 @@ RSpec.describe Metrics::ReviewTurnaroundProcessor, type: :job do
         process_all_events
       end
 
-      # expect metrics_for project id: 'Project A', value: 15.minutes
       it 'updates the review_turnaround metric for the given interval' do
         expect(first_metric).to have_attributes(
           entity_key: 'Project A',
@@ -145,13 +109,7 @@ RSpec.describe Metrics::ReviewTurnaroundProcessor, type: :job do
         expect(first_metric_value_expressed_as_seconds).to be_within(1.second) .of(15.minutes)
       end
 
-      #  event :project, id: 10, action: :created, at: '2019-01-01 15:10:00'
-      #  event :pull_request, id: 1, project: 'Project A', action: :opened,
-      #   at: '2020-01-01 15:10:00'
-      #  event :review, pull_request_id: 1, action: :submitted,
-      #   at: '2020-01-01 15:30:00'
       it 'updates only that metric' do
-        # expect metrics count: 1
         expect(generated_metrics_count).to eq(1)
       end
     end
