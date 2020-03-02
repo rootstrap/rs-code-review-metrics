858d19bf267afc1d260bdebf2917ea9715ebd728
Martin Rubi <martin.rubi@rootstrap.com>
2020-03-02 10:41:57 -0300

    Rewrite process_metrics_job_spec to not use any concrete metric processor

--- spec/jobs/process_metrics_job_spec.rb
+++ spec/jobs/process_metrics_job_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ProcessMetricsJob, type: :job do
   subject { ProcessMetricsJob.new }
 
   it 'creates a Metrics::MetricsProcessor object and calls :call it' do
-    expect_any_instance_of(Metrics::ReviewTurnaroundProcessor).to receive(:call).once
+    expect_any_instance_of(Metrics::MetricsProcessor).to receive(:call).once
 
     subject.perform
   end
