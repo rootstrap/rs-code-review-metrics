1ab45ed5fc10b4d92eb28bace586beead8b2626b
Martin Rubi <martin.rubi@rootstrap.com>
2020-03-04 10:40:41 -0300

    Drop @duration. Use @ending_at instead Use comparison operands instead of :between Use accessors instead of instance variables. Move the accessors above the first method in the class.

--- app/models/time_interval.rb
+++ app/models/time_interval.rb
@@ -1,20 +1,22 @@
 ##
 # An interval of time between two concrete points in time
 class TimeInterval
+  attr_reader :starting_at, :ending_at
+
   ##
   # Creates a TimeInterval starting at the given point in time with the given
-  # duration expressed as a number of seconds
+  # duration expressed as a number of seconds.
+  # The TimeInterval is closed at its starting time and opened at its ending time
+  #
+  #                 [starting_at, ending_at)
   def initialize(starting_at:, duration:)
     @starting_at = starting_at
-    @duration = duration
-    @closed_end_at = @starting_at + @duration - 0.01.seconds
+    @ending_at = @starting_at + duration
   end
-
-  attr_reader :starting_at, :duration
 
   ##
   # Returns true if the TimeInterval includes the given time.
   def includes?(time)
-    time.between?(@starting_at, @closed_end_at)
+    starting_at <= time && time < ending_at
   end
 end
