388833d88e6141a85048e9bf12a67f114e8bd938
Martin Rubi <martin.rubi@rootstrap.com>
2020-03-04 10:28:38 -0300

    Fix typo

--- app/models/time_interval.rb
+++ app/models/time_interval.rb
@@ -2,7 +2,7 @@
 # An interval of time between two concrete points in time
 class TimeInterval
   ##
-  # Creates a TimeInterval stargin at the given point in time with the given
+  # Creates a TimeInterval starting at the given point in time with the given
   # duration expressed as a number of seconds
   def initialize(starting_at:, duration:)
     @starting_at = starting_at
