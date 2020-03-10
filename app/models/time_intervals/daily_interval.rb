08d64bc29a293436c5b1202a07765d2ce592eb20
Martin Rubi <martin.rubi@rootstrap.com>
2020-03-10 12:05:48 -0300

    Fix style

--- app/models/time_intervals/daily_interval.rb
+++ app/models/time_intervals/daily_interval.rb
@@ -8,7 +8,9 @@ module TimeIntervals
   # using the polimorphic :from(starting_at, up_to:) and :next_to(time_interval)
   # methods.
   module DailyInterval
-    def self.each_from(starting_at, up_to:, &iteration_block)
+    extend self
+
+    def each_from(starting_at, up_to:, &iteration_block)
       time_interval = TimeInterval.new(starting_at: starting_at, duration: 1.day)
       while time_interval.starting_at < up_to
         iteration_block.call(time_interval)
@@ -18,11 +20,11 @@ module TimeIntervals
 
     ##
     # Returns the contiguous Daily TimeInterval right next to the given one.
-    def self.next_to(time_interval)
+    def next_to(time_interval)
       at(time_interval.ending_at)
     end
 
-    def self.at(time)
+    def at(time)
       TimeInterval.new(starting_at: time, duration: 1.day)
     end
   end
