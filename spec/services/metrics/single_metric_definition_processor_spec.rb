41afe1ee636d97da8a237eefb77ff92aab74c394
Martin Rubi <martin.rubi@rootstrap.com>
2020-03-11 09:36:46 -0300

    Test that a SingleMetricDefinitionProcessor calls the concrete MetricProcessor to process the events

--- spec/services/metrics/single_metric_definition_processor_spec.rb
+++ spec/services/metrics/single_metric_definition_processor_spec.rb
@@ -13,12 +13,25 @@ RSpec.describe Metrics::SingleMetricDefinitionProcessor do
     end
 
     let(:metrics_definition) { MetricsDefinition.first }
+    let(:events) { Event.all }
 
     context 'with no events to process' do
       it 'does not call any concrete MetricProcessor service' do
         expect_any_instance_of(Metrics::NullProcessor).not_to receive(:call)
 
         subject.call(metrics_definition: metrics_definition, events: [])
+      end
+    end
+
+    context 'with events to process' do
+      before do
+        create :event
+      end
+
+      it 'creates and calls the concrete MetricProcessor service' do
+        expect_any_instance_of(Metrics::NullProcessor).to receive(:call)
+
+        subject.call(metrics_definition: metrics_definition, events: events)
       end
     end
   end
