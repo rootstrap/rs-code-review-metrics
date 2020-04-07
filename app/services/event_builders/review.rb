9f6c372766f125791f2931b037d93002bda849cb
Hernan D'Amico <hernan.damico67@gmail.com>
2020-04-07 18:27:22 -0300

    Code analysis fixes

--- app/services/event_builders/review.rb
+++ app/services/event_builders/review.rb
@@ -5,16 +5,22 @@ module EventBuilders
     def build
       review_data = @payload['review']
       Events::Review.find_or_create_by!(github_id: review_data['id']) do |review|
-        review.owner = find_or_create_user(review_data['user'])
-        review.pull_request = find_pull_request
-        review.review_request = find_or_create_review_request(review.pull_request,
-                                                              review.owner.id)
-        relate_user_project(review.owner, review.pull_request.project)
+        assign_attrs(review, review_data)
 
         ATTR_PAYLOAD_MAP.each do |key, value|
           review.public_send("#{key}=", review_data.fetch(value))
         end
       end
+    end
+
+    def assign_attrs(review, review_data)
+      review.owner = find_or_create_user(review_data['user'])
+      review.pull_request = find_pull_request
+
+      owner = review.owner
+      pull_request = review.pull_request
+      review.review_request = find_or_create_review_request(pull_request, owner.id)
+      owner.projects << pull_request.project
     end
   end
 end
