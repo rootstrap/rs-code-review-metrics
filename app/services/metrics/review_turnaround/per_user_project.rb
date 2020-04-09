3cef6c182609a257dcd590ee0d7a70e8881f3080
Hernán D'Amico <37642427+hdamico@users.noreply.github.com>
2020-04-09 15:24:57 -0300

    Memory performance (#117)
    * Memory performance
    
    * Code analysis fix
    
    * Merge errors

--- app/services/metrics/review_turnaround/per_user_project.rb
+++ app/services/metrics/review_turnaround/per_user_project.rb
@@ -4,15 +4,26 @@ module Metrics
       private
 
       def process
-        review_turnaround_average.each do |review_turnaround|
-          find_or_create_metric(entity: review_turnaround[:entity])
-            .update!(value: review_turnaround[:turnaround],
+        retrieve_reviews.find_each.lazy.each do |review|
+          entity = find_user_project(review.owner, review.pull_request.project)
+          turnaround = calculate_turnaround(review)
+
+          find_or_create_metric(entity: entity)
+            .update!(value: turnaround,
                      value_timestamp: time_interval.starting_at)
         end
       end
 
-      def review_turnaround_average
+      def retrieve_reviews
         Queries::ReviewTurnaround::PerUserProject.call(time_interval: time_interval)
+      end
+
+      def find_user_project(user, project)
+        user.users_projects.detect { |user_project| user_project.project_id == project.id }
+      end
+
+      def calculate_turnaround(review)
+        review.review_request.created_at.to_i - review.opened_at.to_i
       end
     end
   end
--- app/services/queries/review_turnaround/per_user_project.rb
+++ app/services/queries/review_turnaround/per_user_project.rb
@@ -8,26 +8,15 @@ module Queries
       end
 
       def call
-        run
+        execute
       end
 
       private
 
-      def query
+      def execute
         Events::Review.joins(:review_request, owner: :users_projects, pull_request: :project)
                       .includes(:review_request)
                       .where(opened_at: @time_interval.starting_at..@time_interval.ending_at)
-      end
-
-      def run
-        query.lazy.collect do |review|
-          { entity: find_user_project(review.owner, review.pull_request.project),
-            turnaround: review.review_request.created_at.to_i - review.opened_at.to_i }
-        end
-      end
-
-      def find_user_project(user, project)
-        user.users_projects.detect { |user_project| user_project.project_id == project.id }
       end
     end
   end
