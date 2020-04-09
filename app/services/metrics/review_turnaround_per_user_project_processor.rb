b93286f6248a15d62beb11175f24284b23a36af0
Hernan D'Amico <hernan.damico67@gmail.com>
2020-04-09 13:18:33 -0300

    Code analysis fix

--- app/services/metrics/review_turnaround_per_user_project_processor.rb
+++ app/services/metrics/review_turnaround_per_user_project_processor.rb
@@ -5,7 +5,7 @@ module Metrics
     def process
       retrieve_reviews.find_each.lazy.each do |review|
         entity = find_user_project(review.owner, review.pull_request.project)
-        turnaround = (review.review_request.created_at.to_i - review.opened_at.to_i)
+        turnaround = calculate_turnaround(review)
 
         find_or_create_metric(entity: entity)
           .update!(value: turnaround,
@@ -19,6 +19,10 @@ module Metrics
 
     def find_user_project(user, project)
       user.users_projects.detect { |user_project| user_project.project_id == project.id }
+    end
+
+    def calculate_turnaround(review)
+      review.review_request.created_at.to_i - review.opened_at.to_i
     end
   end
 end
