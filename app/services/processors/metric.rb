31b1877f11baae0cdd09fec92a98043fb09adb25
Hernán D'Amico <37642427+hdamico@users.noreply.github.com>
2020-04-15 14:44:14 -0300

    Get daily reviews (#122)
    * Get daily reviews
    
    * Fix

--- app/services/processors/metric.rb
+++ app/services/processors/metric.rb
@@ -13,11 +13,6 @@ module Processors
 
     private
 
-    def update_metric(entity:, value:, value_timestamp:)
-      find_or_create_metric(entity: entity)
-        .update!(value: value, value_timestamp: value_timestamp)
-    end
-
     def find_or_create_metric(entity:)
       ::Metric.find_or_create_by!(ownable: entity,
                                   metrics_definition: metrics_definition)
