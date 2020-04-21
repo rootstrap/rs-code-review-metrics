6399bb4728963a6bf55126aa1a7dd92cac9e8e0b
Hernan D'Amico <hernan.damico67@gmail.com>
2020-04-21 16:05:21 -0300

    Null validation deleted

--- db/migrate/20200318171820_add_project_to_pull_request.rb
+++ db/migrate/20200318171820_add_project_to_pull_request.rb
@@ -1,5 +1,5 @@
 class AddProjectToPullRequest < ActiveRecord::Migration[6.0]
   def change
-    add_reference :pull_requests, :project, null: false, foreign_key: true
+    add_reference :pull_requests, :project, foreign_key: true
   end
 end
