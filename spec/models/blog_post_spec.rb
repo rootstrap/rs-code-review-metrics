2e1d4f903b50862289d28d4c72a398967d1c4cc2
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-04-23 16:39:06 -0300

    Create BlogPost and Technology models

---
+++ spec/models/blog_post_spec.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: blog_posts
+#
+#  id            :bigint           not null, primary key
+#  published_at  :date
+#  slug          :string
+#  status        :string
+#  url           :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  blog_id       :integer
+#  technology_id :bigint
+#
+# Indexes
+#
+#  index_blog_posts_on_technology_id  (technology_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (technology_id => technologies.id)
+#
+require 'rails_helper'
+
+RSpec.describe BlogPost, type: :model do
+end
