95a2bb5d451cb216ca053e1549ed14afe2436ddb
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-04-30 14:32:56 -0300

    Added service to get all posts from Wordpress

---
+++ app/services/wordpress_service.rb
@@ -0,0 +1,61 @@
+class WordpressService
+  def blog_posts(posts: [], next_page_token: nil)
+    response = connection.get(
+      'rest/v1.1/me/posts',
+      { status: BlogPost.statuses[:publish], page_handle: next_page_token },
+      Authorization: "Bearer #{access_token}"
+    )
+    response_body = JSON.parse(response.body).deep_symbolize_keys
+
+    posts += response_body[:posts]
+
+    if response_body[:meta]
+      blog_posts(posts: posts, next_page_token: response_body[:meta][:next_page])
+    else
+      posts
+    end
+  end
+
+  def access_token
+    @access_token ||= get_access_token
+  end
+
+  def get_access_token
+    request_body = {
+      client_id: client_id,
+      client_secret: client_secret,
+      grant_type: 'password',
+      username: username,
+      password: password
+    }
+    response = connection.post('oauth2/token', request_body)
+    response_body = JSON.parse(response.body)
+    raise_invalid_token_error(response_body) if response.status == 400
+
+    response_body['access_token']
+  end
+
+  def connection
+    Faraday.new('https://public-api.wordpress.com/')
+  end
+
+  def raise_invalid_token_error(response_body)
+    raise(Wordpress::InvalidTokenRequestError, response_body['error_description'])
+  end
+
+  def client_id
+    ENV['WORDPRESS_CLIENT_ID']
+  end
+
+  def client_secret
+    ENV['WORDPRESS_CLIENT_SECRET']
+  end
+
+  def username
+    ENV['WORDPRESS_USERNAME']
+  end
+
+  def password
+    ENV['WORDPRESS_PASSWORD']
+  end
+end
---
+++ spec/services/wordpress_service_spec.rb
@@ -0,0 +1,142 @@
+require 'rails_helper'
+
+RSpec.describe WordpressService do
+  describe '#access_token' do
+    let(:client_id) { '11111' }
+    let(:client_secret) { 'qwerty' }
+    let(:username) { 'rootstrap@rootstrap.com' }
+    let(:password) { 'rootstrap' }
+    let(:request_params) do
+      {
+        client_id: ENV['WORDPRESS_CLIENT_ID'],
+        client_secret: ENV['WORDPRESS_CLIENT_SECRET'],
+        grant_type: 'password',
+        username: ENV['WORDPRESS_USERNAME'],
+        password: ENV['WORDPRESS_PASSWORD']
+      }
+    end
+
+    before do
+      stub_env('WORDPRESS_CLIENT_ID', client_id)
+      stub_env('WORDPRESS_CLIENT_SECRET', client_secret)
+      stub_env('WORDPRESS_USERNAME', username)
+      stub_env('WORDPRESS_PASSWORD', password)
+
+      stub_request(:post, 'https://public-api.wordpress.com/oauth2/token')
+        .with(body: request_params)
+        .to_return(body: token_response, status: response_status)
+    end
+
+    context 'when the request is successful' do
+      let(:access_token) { 'asdf' }
+      let(:token_response) { JSON.generate({ access_token: access_token }) }
+      let(:response_status) { 200 }
+
+      it 'returns the access token' do
+        expect(subject.access_token).to eq access_token
+      end
+    end
+
+    context 'when the API fails to return an access token' do
+      let(:password) { 'invalid_password' }
+      let(:error_body) do
+        {
+          'error': 'invalid_request',
+          'error_description': 'Incorrect username or password.'
+        }
+      end
+      let(:token_response) { JSON.generate(error_body) }
+      let(:response_status) { 400 }
+
+      it 'raises an exception' do
+        expect { subject.access_token }.to raise_error Wordpress::InvalidTokenRequestError
+      end
+    end
+  end
+
+  describe '#blog_posts' do
+    let(:access_token) { 'asdf' }
+    let(:authorization_header) { { 'Authorization': "Bearer #{access_token}" } }
+    let(:blog_post) do
+      {
+        'ID': 4424,
+        'site_ID': 166779230,
+        'date': '2020-01-14T19:32:09+00:00',
+        'title': 'Rootstrap named Top Staff Augmentation Company by Clutch',
+        'URL': 'https://www.rootstrap.com/blog/2020/01/14/rootstrap-named-top-staff-augmentation-company-by-clutch/',
+        'short_URL': 'https://www.rootstrap.com/blog/?p=4424',
+        'slug': 'rootstrap-named-top-staff-augmentation-company-by-clutch',
+        'status': 'publish'
+      }
+    end
+    let(:blog_posts_response) do
+      JSON.generate(
+        {
+          'posts': [blog_post],
+          'found': 1
+        }
+      )
+    end
+    let(:request_params) do
+      {
+        status: BlogPost.statuses[:publish]
+      }
+    end
+
+    before do
+      subject.instance_variable_set(:@access_token, access_token)
+      stub_request(:get, 'https://public-api.wordpress.com/rest/v1.1/me/posts')
+        .with(query: request_params.merge({ page_handle: nil }), headers: authorization_header)
+        .to_return(body: blog_posts_response, status: 200)
+    end
+
+    it 'returns the published blog posts of the site' do
+      expect(subject.blog_posts).to contain_exactly(blog_post.deep_symbolize_keys)
+    end
+
+    context 'when there is more than 1 page of results' do
+      let(:next_page_token) { 'value=1564437351&blog=166779230&post=715' }
+      let(:blog_post_2) do
+        {
+          'ID': 686,
+          'site_ID': 166779230,
+          'date': '2019-07-29T12:42:50+00:00',
+          'title': 'DevOps — are your servers pets, cows or ants?',
+          'URL': 'https://www.rootstrap.com/blog/2019/07/29/devops-are-your-servers-pets-cows-or-ants/',
+          'short_URL': 'https://www.rootstrap.com/blog/?p=686',
+          'slug': 'devops-are-your-servers-pets-cows-or-ants',
+          'status': 'publish'
+        }
+      end
+      let(:blog_posts_response) do
+        JSON.generate(
+          {
+            'posts': [blog_post],
+            'found': 2,
+            'meta': {
+              'next_page': next_page_token
+            }
+          }
+        )
+      end
+      let(:blog_posts_response_2) do
+        JSON.generate(
+          {
+            'posts': [blog_post_2],
+            'found': 1
+          }
+        )
+      end
+
+      before do
+        stub_request(:get, 'https://public-api.wordpress.com/rest/v1.1/me/posts')
+          .with(query: request_params.merge({ page_handle: next_page_token }), headers: authorization_header)
+          .to_return(body: blog_posts_response_2, status: 200)
+      end
+
+      it 'returns all the published blog posts of the site' do
+        expect(subject.blog_posts).to match_array([blog_post, blog_post_2].map(&:deep_symbolize_keys))
+      end
+    end
+  end
+end
