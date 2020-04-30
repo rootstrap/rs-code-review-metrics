4d5d29a7ba181bcce0fb6f618cb08b0a90e2ac79
Hernan D'Amico <hernan.damico67@gmail.com>
2020-04-30 17:03:24 -0300

    Spaces

--- clock.rb
+++ clock.rb
@@ -1,5 +1,4 @@
-
-require File.expand_path('../config/boot',        __FILE__)  
+require File.expand_path('../config/boot',        __FILE__)
 require File.expand_path('../config/environment', __FILE__)
 require 'clockwork'
 
