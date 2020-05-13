d744c2cee0f532dd10c218b9cb25790515ddf22c
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-05-13 17:08:24 -0300

    Reduce AbcSize of BlogPostViewsUpdater#call

--- app/services/processors/blog_post_views_updater.rb
+++ app/services/processors/blog_post_views_updater.rb
@@ -6,19 +6,12 @@ module Processors
 
     def call
       blog_post_views[:years].each do |year, year_hash|
-        next if fully_registered_year?(year.to_i)
+        next if fully_registered_year?(year)
 
         year_hash[:months].each do |month, views|
-          next if registered_month?(month.to_i, year.to_i)
+          next if registered_month?(month, year)
 
-          metric = Metric.find_or_initialize_by(
-            ownable: blog_post,
-            name: Metric.names[:blog_visits],
-            interval: Metric.intervals[:monthly],
-            value_timestamp: Time.gm(year, month).end_of_month
-          )
-          metric.value = views
-          metric.save!
+          create_metric(year, month, views)
         end
       end
     end
@@ -27,18 +20,29 @@ module Processors
 
     attr_reader :blog_post_id
 
+    def create_metric(year, month, views)
+      metric = Metric.find_or_initialize_by(
+        ownable: blog_post,
+        name: Metric.names[:blog_visits],
+        interval: Metric.intervals[:monthly],
+        value_timestamp: Time.gm(year, month).end_of_month
+      )
+      metric.value = views
+      metric.save!
+    end
+
     def blog_post_views
       wordpress_service.blog_post_views(blog_post_id)
     end
 
     def fully_registered_year?(year)
-      last_updated_metric.present? && last_updated_year > year
+      last_updated_metric.present? && last_updated_year > year.to_i
     end
 
     def registered_month?(month, year)
       last_updated_metric.present? &&
-        last_updated_month > month &&
-        last_updated_year == year
+        last_updated_month > month.to_i &&
+        last_updated_year == year.to_i
     end
 
     def last_updated_year
