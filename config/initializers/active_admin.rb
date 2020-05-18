5f67e4da4443646e922816af8789245cfc623a1a
Hernan D'Amico <hernan.damico67@gmail.com>
2020-05-18 18:40:24 -0300

    Code analysis

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -325,7 +325,7 @@ ActiveAdmin.setup do |config|
   # You can inherit it with own class and inject it for all resources
   #
   # config.order_clause = MyOrderClause
-  
+
   # == Webpacker
   #
   # By default, Active Admin uses Sprocket's asset pipeline.
