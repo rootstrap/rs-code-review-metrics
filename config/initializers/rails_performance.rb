23b83e2440935b048da5785c747a93904b34159c
Hernan D'Amico <hernan.damico67@gmail.com>
2020-05-18 11:12:01 -0300

    Fix

--- config/initializers/rails_performance.rb
+++ config/initializers/rails_performance.rb
@@ -3,6 +3,10 @@ if defined?(RailsPerformance)
     config.redis    = Redis.new(url: ENV.fetch('REDIS_PERFORMANCE_URL') { 'redis://localhost:6379/1' })
     config.duration = 4.hours
     config.enabled  = true
-    config.http_basic_authentication_enabled = false
+    if Rails.env.production?
+      config.http_basic_authentication_enabled = true
+      config.http_basic_authentication_user_name = ENV.fetch('PERFORMANCE_USERNAME')
+      config.http_basic_authentication_password = ENV.fetch('PERFORMANCE_PASSWORD')
+    end
   end
 end
