6ddc4b93535165e40bdb904ac125573e7ade33e7
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-05-27 14:18:12 -0300

    Improve query performance in BlogMetricsUpdater

--- app/services/processors/blog_metrics_updater.rb
+++ app/services/processors/blog_metrics_updater.rb
@@ -8,7 +8,7 @@ module Processors
     private
 
     def update_blog_post_visits_metrics
-      BlogPost.find_each(batch_size: 50) do |blog_post|
+      BlogPost.find_each(batch_size: 50).lazy.each do |blog_post|
         BlogPostViewsUpdater.call(blog_post.blog_id)
       end
     end
