4ade9010a78a9c367de3851605c96f0a481b6b69
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-06-02 10:55:09 -0300

    Schedule Blog jobs

---
+++ spec/jobs/blog_metrics_full_updater_job_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe BlogMetricsFullUpdaterJob do
+  describe '#perform' do
+    it 'correctly initializes the BlogMetricsFullUpdater processor and calls it' do
+      expect_any_instance_of(Processors::BlogMetricsFullUpdater)
+        .to receive(:call)
+        .once
+
+      described_class.perform_now
+    end
+  end
+end
