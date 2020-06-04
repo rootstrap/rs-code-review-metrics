3c924141d07847d661ab57663862d92edbe42a9f
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-06-04 12:43:33 -0300

    Create blog post count metrics for months without new blog posts

--- app/services/processors/blog_post_count_metrics_updater.rb
+++ app/services/processors/blog_post_count_metrics_updater.rb
@@ -2,9 +2,7 @@ module Processors
   class BlogPostCountMetricsUpdater < BaseService
     def call
       Technology.all.each do |technology|
-        new_blog_posts_by_month = technology.blog_posts.group_by_month(:published_at).count
-
-        new_blog_posts_by_month.each do |month, new_blog_post_count|
+        new_blog_posts_by_month_for(technology).each do |month, new_blog_post_count|
           timestamp = month.to_time.utc
           cumulative_blog_post_count = new_blog_post_count +
                                        registered_blog_count_for(technology, timestamp.last_month)
@@ -19,6 +17,17 @@ module Processors
     end
 
     private
+
+    def new_blog_posts_by_month_for(technology)
+      technology
+        .blog_posts
+        .group_by_month(:published_at, range: starting_time_to_update..Time.zone.now)
+        .count
+    end
+
+    def starting_time_to_update
+      BlogPost.pluck(:published_at).min
+    end
 
     def create_or_update_blog_post_count_metric(technology, timestamp, blog_post_count)
       metric = Metric.find_or_initialize_by(
--- spec/services/processors/blog_post_count_metrics_updater_spec.rb
+++ spec/services/processors/blog_post_count_metrics_updater_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe Processors::BlogPostCountMetricsUpdater do
   describe '.call' do
     let(:technology) { create(:technology) }
-    let!(:blog_post) do
-      create(:blog_post, published_at: Time.zone.now, technology: technology)
+    let(:publish_time) { Time.zone.now }
+
+    before do
+      create(:blog_post, published_at: publish_time, technology: technology)
     end
 
     it 'creates a blog post count metric for that technology' do
@@ -20,8 +22,8 @@ RSpec.describe Processors::BlogPostCountMetricsUpdater do
     end
 
     context 'having more than one technology' do
-      let(:other_technology) { create(:technology) }
-      let!(:other_blog_post) do
+      before do
+        other_technology = create(:technology)
         create(:blog_post, published_at: Time.zone.now, technology: other_technology)
       end
 
@@ -51,7 +53,7 @@ RSpec.describe Processors::BlogPostCountMetricsUpdater do
     end
 
     context 'when there are blog posts published in different months' do
-      let!(:older_blog_post) do
+      before do
         create(:blog_post, published_at: Time.zone.now.last_month, technology: technology)
       end
 
@@ -77,6 +79,67 @@ RSpec.describe Processors::BlogPostCountMetricsUpdater do
 
         expect(last_month_metric.value).to eq 1
         expect(this_month_metric.value).to eq 2
+      end
+    end
+
+    context 'when there is a month without new blog posts' do
+      context 'and it is the latest' do
+        let(:empty_month_timestamp) { Time.zone.now }
+        let(:publish_time) { empty_month_timestamp.last_month }
+
+        it 'adds the metric for that month with the unchanged value' do
+          described_class.call
+
+          empty_month_metric = Metric.find_by!(
+            name: Metric.names[:blog_post_count],
+            interval: Metric.intervals[:monthly],
+            ownable: technology,
+            value_timestamp: empty_month_timestamp.end_of_month
+          )
+          expect(empty_month_metric.value).to eq 1
+        end
+      end
+
+      context 'and it is between other months with new blog posts' do
+        let(:empty_month_timestamp) { Time.zone.now.last_month }
+        let(:publish_time) { empty_month_timestamp.last_month }
+
+        before do
+          create(:blog_post, published_at: empty_month_timestamp.next_month, technology: technology)
+        end
+
+        it 'adds the metric for that month with the unchanged value' do
+          described_class.call
+
+          empty_month_metric = Metric.find_by!(
+            name: Metric.names[:blog_post_count],
+            interval: Metric.intervals[:monthly],
+            ownable: technology,
+            value_timestamp: empty_month_timestamp.end_of_month
+          )
+          expect(empty_month_metric.value).to eq 1
+        end
+      end
+
+      context 'and it is at the start of the period queried' do
+        let(:empty_month_timestamp) { Time.zone.now.last_month }
+
+        before do
+          other_technology = create(:technology)
+          create(:blog_post, published_at: empty_month_timestamp, technology: other_technology)
+        end
+
+        it 'adds the metric for that month with the unchanged value' do
+          described_class.call
+
+          empty_month_metric = Metric.find_by!(
+            name: Metric.names[:blog_post_count],
+            interval: Metric.intervals[:monthly],
+            ownable: technology,
+            value_timestamp: empty_month_timestamp.end_of_month
+          )
+          expect(empty_month_metric.value).to eq 0
+        end
       end
     end
   end
