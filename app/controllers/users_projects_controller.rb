8b2a94ff765e04ad79415cad53b80219d1b39a38
Hernan D'Amico <hernan.damico67@gmail.com>
2020-06-04 16:51:58 -0300

    Code climate errors

--- app/controllers/users_projects_controller.rb
+++ app/controllers/users_projects_controller.rb
@@ -1,28 +1,2 @@
-class UsersProjectsController < ApplicationController
-  def metrics
-    return if metric_params.blank?
-
-    period_metric_query = Metrics::PeriodRetriever.call(metric_params[:period])
-
-    @review_turnaround = period_metric_query.call(
-      entity_name: controller_name, entity_id: project_id, metric_name: 'review_turnaround'
-    )
-    @merge_time = period_metric_query.call(
-      entity_name: controller_name, entity_id: project_id, metric_name: 'merge_time'
-    )
-  end
-
-  private
-
-  def project_id
-    @project_id ||= Project.find_by(name: params[:project_name]).id
-  end
-
-  def controller_name
-    @controller_name ||= params[:controller]
-  end
-
-  def metric_params
-    @metric_params ||= params[:metric]
-  end
+class UsersProjectsController < MetricsController
 end
