46bf712ff989ecc718d64b6417475fc867ac6d97
Hernan D'Amico <hernan.damico67@gmail.com>
2020-06-10 17:34:20 -0300

    Fix

--- app/services/builders/chartkick/project_data.rb
+++ app/services/builders/chartkick/project_data.rb
@@ -2,7 +2,7 @@ module Builders
   module Chartkick
     class ProjectData < Builders::Chartkick::Base
       def call
-        project = Project.find(@entity_id)
+        project = ::Project.find(@entity_id)
         metrics = project.metrics.where(@query)
         [{ name: project.name, data: build_data(metrics) }]
       end
