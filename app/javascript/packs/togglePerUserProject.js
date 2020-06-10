2a01fbdedc3a11c3bdc1503284fcfc5bb51252bc
Hernán D'Amico <37642427+hdamico@users.noreply.github.com>
2020-06-10 14:57:43 -0300

    Update variable type
    Co-authored-by: Hosward Villoria <hosward.avc@gmail.com>

--- app/javascript/packs/togglePerUserProject.js
+++ app/javascript/packs/togglePerUserProject.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 });
 
 $(document).on('turbolinks:load', function() {
-  var canvas = document.getElementsByTagName("canvas").length;
+  const canvas = document.getElementsByTagName("canvas").length;
   if(canvas === 0) {
     $("button").hide();
   } else {
