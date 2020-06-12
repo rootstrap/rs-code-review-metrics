0f61c23b8fdb8f5dac96b787e00f08736a09e188
Martin Rubi <martin.rubi@rootstrap.com>
2020-06-12 15:12:31 -0300

    Replace method with constant

--- spec/services/code_climate/code_climate_spec_helper.rb
+++ spec/services/code_climate/code_climate_spec_helper.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 module CodeClimateSpecHelper
-  def base_url
-    'https://api.codeclimate.com/v1'
-  end
+  CODE_CLIMATE_API_URL = ENV['CODE_CLIMATE_API_URL']
 
   def on_request_repository(project_name:, respond:)
-    stub_request(:get, "#{base_url}/repos?github_slug=rootstrap/#{project_name}")
+    stub_request(:get, "#{CODE_CLIMATE_API_URL}/repos?github_slug=rootstrap/#{project_name}")
       .to_return(status: respond[:status], body: JSON.generate(respond[:body]))
   end
 
   def on_request_snapshot(repo_id:, snapshot_id:, respond:)
-    stub_request(:get, "#{base_url}/repos/#{repo_id}/snapshots/#{snapshot_id}")
+    stub_request(:get, "#{CODE_CLIMATE_API_URL}/repos/#{repo_id}/snapshots/#{snapshot_id}")
       .to_return(status: respond[:status], body: JSON.generate(respond[:body]))
   end
 
   def on_request_issues(repo_id:, snapshot_id:, respond:)
-    stub_request(:get, "#{base_url}/repos/#{repo_id}/snapshots/#{snapshot_id}/issues")
+    stub_request(:get, "#{CODE_CLIMATE_API_URL}/repos/#{repo_id}/snapshots/#{snapshot_id}/issues")
       .to_return(status: respond[:status], body: JSON.generate(respond[:body]))
   end
 end
