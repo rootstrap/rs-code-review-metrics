95b99674e6f93031dd15771b1ac1e247189e52aa
Martin Rubi <martin.rubi@rootstrap.com>
2020-06-23 13:03:50 -0300

    Use department

--- app/services/code_climate/projects_summary_service.rb
+++ app/services/code_climate/projects_summary_service.rb
@@ -30,19 +30,19 @@ module CodeClimate
     end
 
     def invalid_issues_count_average
-      return if code_climate_metrics.empty?
+      return unless metrics?
 
       code_climate_metrics.map(&:invalid_issues_count).sum / code_climate_metrics.size
     end
 
     def wont_fix_issues_count_average
-      return if code_climate_metrics.empty?
+      return unless metrics?
 
       code_climate_metrics.map(&:wont_fix_issues_count).sum / code_climate_metrics.size
     end
 
     def open_issues_count_average
-      return if code_climate_metrics.empty?
+      return unless metrics?
 
       code_climate_metrics.map(&:open_issues_count).sum / code_climate_metrics.size
     end
