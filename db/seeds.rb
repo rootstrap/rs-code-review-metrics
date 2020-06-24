9957d2c550f588210d50a9ed6ea4668177e6ab79
Hernan D'Amico <hernan.damico67@gmail.com>
2020-06-24 11:21:36 -0300

    Fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -75,20 +75,20 @@ end
 if Rails.env.test?
   ActiveRecord::Base.transaction do
     # Departments and Languages
-    department = Department.create(name: 'backend')
+    department = FactoryBot.create(:department, name: 'backend')
     %i[ruby nodejs python].each do |lang|
-      Language.create(name: lang, department: department)
+      FactoryBot.create(:language, name: lang, department: department)
     end
-    department = Department.create(name: 'frontend')
+    department = FactoryBot.create(:department, 'frontend')
     %i[react vuejs].each do |lang|
-      Language.create(name: lang, department: department)
+      FactoryBot.create(:language, name: lang, department: department)
     end
-    department = Department.create(name: 'mobile')
+    department = FactoryBot.create(:department, 'mobile')
     %i[ios android react_native].each do |lang|
-      Language.create(name: lang, department: department)
+      FactoryBot.create(:language, name: lang, department: department)
     end
     %i[others unassigned].each do |lang|
-      Language.create(name: lang)
+      FactoryBot.create(:language, name: lang)
     end
   end
 end
