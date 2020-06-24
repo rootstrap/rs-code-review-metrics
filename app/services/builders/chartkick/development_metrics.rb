e44b1622c1e212aa34d2b95686021cbd86141ad2
Hernan D'Amico <hernan.damico67@gmail.com>
2020-06-24 12:19:17 -0300

    Fix

--- app/services/builders/chartkick/development_metrics.rb
+++ app/services/builders/chartkick/development_metrics.rb
@@ -14,41 +14,47 @@ module Builders
 
       private
 
+      def build
+        METRIC_NAMES.inject({}) do |hash, metric_name|
+          hash[metric_name] = send("#{resolve_entity_name}_metric_data", metric_name)
+        end
+      end
+
       def period_metric_query(period)
         Metrics::PeriodRetriever.call(period)
+      end
+
+      def resolve_entity_name
+        self.class.name.demodulize.downcase
       end
 
       class Project < DevelopmentMetrics
         private
 
-        def build
-          METRIC_NAMES.each_with_object({}) do |metric_name, hash|
-            hash[metric_name] = {
-              per_project: @period.call(
-                entity_name: 'project', entity_id: @entity_id, metric_name: metric_name
-              ),
-              per_users_project: @period.call(
-                entity_name: 'users_project', entity_id: @entity_id, metric_name: metric_name
-              )
-            }
-          end
+        def project_metric_data(metric_name)
+          {
+            per_project: @period.call(
+              entity_name: 'project', entity_id: @entity_id, metric_name: metric_name
+            ),
+            per_users_project: @period.call(
+              entity_name: 'users_project', entity_id: @entity_id, metric_name: metric_name
+            )
+          }
         end
       end
 
       class Department < DevelopmentMetrics
         private
 
-        def build
-          METRIC_NAMES.each_with_object({}) do |metric_name, hash|
-            hash[metric_name] = {
-              per_department: @period.call(
-                entity_name: 'department', entity_id: @entity_id, metric_name: metric_name
-              ),
-              per_language: @period.call(
-                entity_name: 'language', entity_id: @entity_id, metric_name: metric_name
-              )
-            }
-          end
+        def department_metric_data(metric_name)
+          {
+            per_department: @period.call(
+              entity_name: 'department', entity_id: @entity_id, metric_name: metric_name
+            ),
+            per_language: @period.call(
+              entity_name: 'language', entity_id: @entity_id, metric_name: metric_name
+            )
+          }
         end
       end
     end
