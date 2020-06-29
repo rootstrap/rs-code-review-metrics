95617f02aa422c5c74411a77a35843eea85cb6d4
Hernan D'Amico <hernan.damico67@gmail.com>
2020-06-29 12:24:32 -0300

    Fix

--- app/services/metrics/merge_time/per_department.rb
+++ app/services/metrics/merge_time/per_department.rb
@@ -13,8 +13,7 @@ module Metrics
 
       def project_metrics_per_department
         Department.joins(languages: { projects: :metrics })
-                  .where(metrics: { name: :merge_time })
-                  .where(metrics: { created_at: metric_interval })
+                  .where(metrics: { name: :merge_time, created_at: metric_interval })
                   .group(:id)
                   .pluck(:id, Arel.sql('COUNT(*), SUM(metrics.value)'))
       end
