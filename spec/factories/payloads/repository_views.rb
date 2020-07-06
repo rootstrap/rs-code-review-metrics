8f5b9a2f59d9c02c190524081a493a5ae47ae259
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-07-06 16:32:33 -0300

    Generate weekly metrics for Open Source visits

--- spec/factories/payloads/repository_views.rb
+++ spec/factories/payloads/repository_views.rb
@@ -6,9 +6,12 @@ FactoryBot.define do
     uniques { Faker::Number.number(digits: 3) }
 
     views do
-      start_date = 14.days.ago.to_date
-      end_date = Time.zone.today.to_date
-      (start_date..end_date).map(&:beginning_of_day).map do |timestamp|
+      now = Time.zone.now
+      [
+        now - 2.weeks,
+        now.last_week,
+        now
+      ].map(&:beginning_of_week).map do |timestamp|
         {
           'timestamp': timestamp.iso8601,
           'count': Faker::Number.number(digits: 2),
