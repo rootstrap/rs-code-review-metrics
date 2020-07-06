03238dbe64baebaa33ef43ab4770ca6cee779c00
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-07-06 14:41:31 -0300

    Minor changes to GrowthMonthOverMonth builder

--- app/services/builders/metric_chart/blog/growth_month_over_month.rb
+++ app/services/builders/metric_chart/blog/growth_month_over_month.rb
@@ -11,14 +11,14 @@ module Builders
         def process_data(metrics_data)
           metrics_data.each_with_object({}) do |(month, metric_value), hash|
             previous_metric_value = metrics_data[month.last_month]
-            next(hash) if previous_metric_value.nil? || previous_metric_value.zero?
+            next(hash) unless previous_metric_value&.positive?
 
             hash[month] = growth_for(metric_value, previous_metric_value)
           end
         end
 
         def growth_for(metric_value, previous_metric_value)
-          (metric_value - previous_metric_value) / previous_metric_value * 100
+          (metric_value - previous_metric_value) * 100 / previous_metric_value
         end
       end
     end
