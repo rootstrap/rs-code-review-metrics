cf7fc73e4875d5b362f8d7bf7c0f7fccdcf5e2ed
Hosward <hosward.avc@gmail.com>
2020-07-14 12:48:38 -0300

    reduces amount of parameters

--- app/services/code_climate/project_summary.rb
+++ app/services/code_climate/project_summary.rb
@@ -3,15 +3,13 @@ module CodeClimate
     attr_reader :rate, :invalid_issues_count, :open_issues_count,
                 :wont_fix_issues_count, :snapshot_time, :name
 
-    def initialize(rate:, invalid_issues_count:, open_issues_count:,
-                   wont_fix_issues_count:, snapshot_time:, name:)
-
-      @rate = rate
-      @invalid_issues_count = invalid_issues_count
-      @open_issues_count = open_issues_count
-      @wont_fix_issues_count = wont_fix_issues_count
-      @snapshot_time = snapshot_time
-      @name = name
+    def initialize(metric:, project_name:)
+      @rate = metric.code_climate_rate
+      @invalid_issues_count = metric.invalid_issues_count
+      @open_issues_count = metric.open_issues_count
+      @wont_fix_issues_count = metric.wont_fix_issues_count
+      @snapshot_time = metric.snapshot_time
+      @name = project_name
     end
   end
 end
