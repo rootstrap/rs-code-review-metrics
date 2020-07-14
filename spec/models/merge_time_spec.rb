3fe99e2a3794b63cb483ec283315053bac1aaa23
Hernan D'Amico <hernan.damico67@gmail.com>
2020-07-14 16:04:36 -0300

    Minor changes

--- spec/models/merge_time_spec.rb
+++ spec/models/merge_time_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: merge_times
+#
+#  id              :bigint           not null, primary key
+#  value           :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  pull_request_id :bigint           not null
+#
+# Indexes
+#
+#  index_merge_times_on_pull_request_id  (pull_request_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (pull_request_id => pull_requests.id)
+#
+
 require 'rails_helper'
 
 RSpec.describe MergeTime, type: :model do
