7d42d7d07e22fd86d7060b7658c7302a1bdfc273
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-07-21 14:08:29 -0300

    Show Open Source project count

--- app/controllers/open_source_controller.rb
+++ app/controllers/open_source_controller.rb
@@ -1,11 +1,17 @@
 class OpenSourceController < ApplicationController
   def index
     @visits_per_language = visits_per_language
+    @projects_per_language = projects_per_language
+    @total_projects = @projects_per_language.values.sum
   end
 
   private
 
   def visits_per_language
     Builders::MetricChart::OpenSource::LanguageVisits.call
+  end
+
+  def projects_per_language
+    Builders::ChartSummary::LifetimeOpenSourceProjectCount.call
   end
 end
