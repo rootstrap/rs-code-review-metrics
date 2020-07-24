2bcd7e926a7917ba65b958aaed2df9b06541ba35
Hosward <hosward.avc@gmail.com>
2020-07-24 12:25:02 -0300

    find the file content in three locations

--- app/services/code_owners/request.rb
+++ app/services/code_owners/request.rb
@@ -2,8 +2,8 @@ module CodeOwners
   class Request < BaseService
     def call
       Project.find_each do |project|
-        content_file = GithubRepositoryClient.new(project.name).get_content_from_file('CODEOWNERS')
-        next if content_file.empty?
+        content_file = GithubRepositoryClient.new(project.name).code_owners
+        next if content_file.nil?
 
         code_owners = CodeOwners::FileHandler.call(content_file)
         CodeOwners::ProjectHandler.call(project, code_owners)
