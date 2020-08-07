ed513a8f1e79727088f9e66510d6a5f7b85e43a0
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-08-07 13:25:02 -0300

    Enable BlogPostsUpdater to finish correctly even with API errors

--- app/services/processors/blog_posts_updater.rb
+++ app/services/processors/blog_posts_updater.rb
@@ -2,13 +2,12 @@ module Processors
   class BlogPostsUpdater < BaseService
     def call
       blog_posts.each do |blog_post_payload|
-        blog_post_id = blog_post_payload[:ID]
-        blog_post = BlogPost.find_or_initialize_by(blog_id: blog_post_id)
+        blog_post = BlogPost.find_or_initialize_by(blog_id: blog_post_payload[:ID])
         blog_post.published_at = blog_post_payload[:date]
         blog_post.slug = blog_post_payload[:slug]
         blog_post.status = blog_post_payload[:status]
         blog_post.url = blog_post_payload[:URL]
-        blog_post.technologies = technologies_for(blog_post_id)
+        blog_post.technologies = technologies_for(blog_post)
 
         blog_post.save!
       end
@@ -24,9 +23,12 @@ module Processors
       @categorizer ||= BlogPostCategorizer.new
     end
 
-    def technologies_for(blog_post_id)
-      blog_post_payload = wordpress_service.blog_post(blog_post_id)
+    def technologies_for(blog_post)
+      blog_post_payload = wordpress_service.blog_post(blog_post.blog_id)
       categorizer.technologies_for(blog_post_payload)
+    rescue Faraday::Error => exception
+      ExceptionHunter.track(exception)
+      blog_post.technologies
     end
   end
 end
--- spec/services/processors/blog_posts_updater_spec.rb
+++ spec/services/processors/blog_posts_updater_spec.rb
@@ -4,22 +4,42 @@ RSpec.describe Processors::BlogPostsUpdater do
   describe '.call' do
     subject { Processors::BlogPostsFullUpdater.call }
 
+    let(:blog_post_payload) { create(:blog_post_payload) }
+
     before do
       create(:technology, name: 'other')
-      blog_post_payload = create(:blog_post_payload)
       stub_blog_posts_response(blog_post_payloads: [blog_post_payload])
-      stub_successful_blog_post_response(blog_post_payload)
     end
 
-    it 'saves all blog posts in the db' do
-      expect { subject }.to change(BlogPost, :count).by(1)
+    context 'when all requests succeed' do
+      before do
+        stub_successful_blog_post_response(blog_post_payload)
+      end
+
+      it 'saves all blog posts in the db' do
+        expect { subject }.to change(BlogPost, :count).by(1)
+      end
+
+      context 'when the post has already been imported' do
+        before { subject }
+
+        it 'does not create another blog post' do
+          expect { subject }.not_to change(BlogPost, :count)
+        end
+      end
     end
 
-    context 'when the post has already been imported' do
-      before { subject }
+    context 'when the request to get a blog post full payload fails' do
+      before { stub_failed_blog_post_response(blog_post_payload['ID']) }
 
-      it 'does not create another blog post' do
-        expect { subject }.not_to change(BlogPost, :count)
+      it 'creates the blog post anyway' do
+        expect { subject }.to change(BlogPost, :count).by(1)
+      end
+
+      it 'notifies the error to exception hunter' do
+        expect(ExceptionHunter).to receive(:track).with(kind_of(Faraday::Error))
+
+        subject
       end
     end
   end
