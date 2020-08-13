e44b8fde5bf2d58081b9af2da514e770e9a37c30
Hosward <hosward.avc@gmail.com>
2020-08-13 12:09:14 -0300

    testing commit

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -19,7 +19,7 @@
 
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable,
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 end
