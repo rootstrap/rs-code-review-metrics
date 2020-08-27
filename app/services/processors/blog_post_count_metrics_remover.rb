d4b730feeb409696379e3110e227ac14d95eab7b
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-08-27 14:52:01 -0300

    Remove code for deprecating blog post count metrics

--- app/services/processors/blog_post_count_metrics_remover.rb
+++
@@ -1,9 +0,0 @@
-module Processors
-  class BlogPostCountMetricsRemover < Processors::Base
-    private
-
-    def process
-      Metric.where(name: Metric.names[:blog_post_count]).destroy_all
-    end
-  end
-end
