2ded7a7befa1d84f63d20c08b386eb63554cd8a8
Sandro Damilano <sandro.damilano@rootstrap.com>
2020-09-11 16:55:31 -0300

    Increase batch size for PullRequest querying in PullRequestSizeUpdater

--- app/services/processors/pull_request_size_updater.rb
+++ app/services/processors/pull_request_size_updater.rb
@@ -1,7 +1,7 @@
 module Processors
   class PullRequestSizeUpdater < BaseService
     def call
-      Events::PullRequest.find_each(batch_size: 100).lazy.each do |pull_request|
+      Events::PullRequest.find_each.lazy.each do |pull_request|
         Builders::PullRequestSize.call(pull_request)
       end
     end
