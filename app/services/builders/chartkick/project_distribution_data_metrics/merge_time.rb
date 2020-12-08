40320806d9a1ff2192a3878856549c1b75de6791
Sebastián Caraballo <sebastian.caraballo@rootstrap.com>
2020-12-08 15:54:06 -0300

    Distribution graph

---
+++ app/services/builders/chartkick/project_distribution_data_metrics/merge_time.rb
@@ -0,0 +1,18 @@
+module Builders
+  module Chartkick
+    module ProjectDistributionDataMetrics
+      class MergeTime
+        def retrieve_records(entity_id:, time_range:)
+          ::MergeTime
+            .joins(pull_request: :project)
+            .where(projects: { id: entity_id })
+            .where(pull_requests: { merged_at: time_range })
+        end
+
+        def resolve_interval(entity)
+          Metrics::IntervalResolver::Time.call(entity.value_as_hours)
+        end
+      end
+    end
+  end
+end
