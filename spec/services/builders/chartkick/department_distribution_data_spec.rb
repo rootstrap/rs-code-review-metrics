a5e77a44e8f4c6ab2c957c5bb9fc3eeb35c92167
Sebastián Caraballo <sebastian.caraballo@rootstrap.com>
2020-12-09 15:31:14 -0300

    Add Department MT and RT success rates

--- spec/services/builders/chartkick/department_distribution_data_spec.rb
+++ spec/services/builders/chartkick/department_distribution_data_spec.rb
@@ -24,12 +24,42 @@ RSpec.describe Builders::Chartkick::DepartmentDistributionData do
         let(:metric_name) { :merge_time }
 
         it_behaves_like 'merge time data distribution'
+        it_behaves_like 'success rate'
       end
 
       context 'when name is review turnaround' do
         let(:metric_name) { :review_turnaround }
 
-        it_behaves_like 'review turnaround data distribution'
+        before do
+          values_in_seconds.each do |value|
+            review_request = create :review_request, project: project
+            create(:completed_review_turnaround, review_request: review_request, value: value)
+          end
+        end
+
+        it 'returns an array with name key' do
+          expect(subject.first).to have_key(:name)
+        end
+
+        it 'returns an array with size of number of values' do
+          expect(subject.first[:data]).to have_exactly(6).items
+        end
+
+        it 'returns an array with one value matched in every position' do
+          subject.first[:data].each do |data_array|
+            expect(data_array.second).to eq(1)
+          end
+        end
+
+        it 'returns an array with name data' do
+          expect(subject.first).to have_key(:data)
+        end
+
+        it 'returns an array with filled value' do
+          expect(subject.first[:data].empty?).to be false
+        end
+
+        it_behaves_like 'success rate'
       end
 
       context 'when name is pull request size' do
