945cdfff28d2710e4ca706b93809c99fbb1d92f7
Enzo Fabbiani <enzofab91@hotmail.com>
2021-07-01 13:03:47 -0300

    Fix EDR when several bugs for same environment

--- spec/services/metrics/defect_escape_rate/per_project_spec.rb
+++ spec/services/metrics/defect_escape_rate/per_project_spec.rb
@@ -1,6 +1,4 @@
-require 'rails_helper'
-
-RSpec.describe Metrics::DefectEscapeRate::PerProject do
+describe Metrics::DefectEscapeRate::PerProject do
   describe '.call' do
     let(:project) { create(:project) }
     let!(:jira_project) { create(:jira_project, project: project) }
@@ -8,12 +6,12 @@ RSpec.describe Metrics::DefectEscapeRate::PerProject do
     let(:subject) { described_class.call(project.id) }
 
     context 'when there are bugs for the project' do
-      let!(:jira_bug) do
-        create(:jira_issue,
-               :bug,
-               :production,
-               jira_project: jira_project,
-               informed_at: beginning_of_day)
+      let!(:jira_bugs) do
+        create_list(:jira_issue, rand(1..10),
+                    :bug,
+                    :production,
+                    jira_project: jira_project,
+                    informed_at: beginning_of_day)
       end
 
       it 'returns the expected payload' do
@@ -23,7 +21,8 @@ RSpec.describe Metrics::DefectEscapeRate::PerProject do
         expect(metrics.count).to eq(1)
         expect(metric.ownable_id).to eq(project.id)
         expect(metric.value_timestamp).to eq(beginning_of_day)
-        expect(metric.value).to eq(defect_rate: 100, bugs_by_environment: { 'production' => 1 })
+        expect(metric.value).to eq(defect_rate: 100,
+                                   bugs_by_environment: { 'production' => jira_bugs.count })
       end
     end
 
