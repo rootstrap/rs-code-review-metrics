aa6860efea53142a4856c486f60e51361d27a569
Enzo Fabbiani <enzofab91@hotmail.com>
2021-09-13 12:34:02 -0300

    Add success rate for repository metrics

--- app/services/builders/chartkick/repository_distribution_data.rb
+++ app/services/builders/chartkick/repository_distribution_data.rb
@@ -1,13 +1,34 @@
 module Builders
   module Chartkick
     class RepositoryDistributionData < Builders::Chartkick::Base
+      REPOSITORY = 'repository'.freeze
+
       def call
         repository_name = ::Repository.find(@entity_id).name
 
-        [{ name: repository_name, data: build_distribution_data(retrieve_records) }]
+        [{
+          name: repository_name,
+          data: build_distribution_data(retrieve_records),
+          success_rate: build_success_rate(retrieve_records)
+        }]
       end
 
       private
+
+      def build_success_rate(entities)
+        intervals = build_distribution_data(entities)
+
+        detail = Builders::Chartkick::Helpers::SuccessRate.call(REPOSITORY,
+                                                                metric_name,
+                                                                intervals)
+
+        return unless detail
+
+        { rate: detail.rate,
+          successful: detail.successful,
+          total: detail.total,
+          metric_detail: detail.metric_detail }
+      end
 
       def retrieve_records
         metric.retrieve_records(entity_id: @entity_id, time_range: @query[:value_timestamp])
