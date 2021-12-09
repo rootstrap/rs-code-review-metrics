4d8c6eef3c3deae4ef190c12c1504cbb6986026e
bruno <brunoparma@hotmail.com>
2021-12-09 10:51:37 -0300

    change bootstrap min

--- app/javascript/packs/bootstrap.bundle.js
+++ app/javascript/packs/bootstrap.bundle.js
@@ -1,4 +1,4 @@
-/* eslint-enable */
+/* eslint-disabled */
 /*!
   * Bootstrap v5.0.2 (https://getbootstrap.com/)
   * Copyright 2011-2021 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
