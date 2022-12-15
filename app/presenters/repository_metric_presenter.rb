7a3470fa45b1ceba8860a3c0c6237723c1c5c223
Daniel Serrano <daniel.serrano@koombea.com>
2022-12-15 15:58:44 +0100

    use attr_reader in meta method

--- app/presenters/repository_metric_presenter.rb
+++ app/presenters/repository_metric_presenter.rb
@@ -2,7 +2,7 @@ class RepositoryMetricPresenter
   METRIC_FILTERS = %i[per_repository per_users_repository per_repository_distribution].freeze
   METRIC_FILTERS.each do |filter|
     define_method("#{filter}_has_data_to_display?") do
-      content = @metric.dig(filter, 0, :data)
+      content = metric.dig(filter, 0, :data)
       content.present?
     end
   end
