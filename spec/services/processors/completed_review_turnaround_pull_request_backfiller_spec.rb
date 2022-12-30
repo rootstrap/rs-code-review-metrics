0da5658ddb1b1d62f90f9a61d5ef327e5f06de63
Daniel Serrano <daniel.serrano@koombea.com>
2022-12-30 11:10:58 +0100

    rubocop styling changes

--- spec/services/processors/completed_review_turnaround_pull_request_backfiller_spec.rb
+++ spec/services/processors/completed_review_turnaround_pull_request_backfiller_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Processors::CompletedReviewTurnaroundPullRequestBackfiller do
   describe '.call' do
-
     let(:ruby_lang) { Language.find_by(name: 'ruby') }
     let!(:repository) { create(:repository, language: ruby_lang) }
     let!(:review_request) { create(:review_request, repository: repository) }
@@ -14,9 +13,11 @@ RSpec.describe Processors::CompletedReviewTurnaroundPullRequestBackfiller do
 
     before { completed_review_turnaround.save!(validate: false) }
     it 'updates pull_request foreign_key in the completed review turnaround records' do
-      expect { described_class.call }.to change { completed_review_turnaround.reload.pull_request_id }
-                                           .from(nil)
-                                           .to(completed_review_turnaround.review_request.pull_request_id)
+      expect { described_class.call }.to change {
+                                           completed_review_turnaround.reload.pull_request_id
+                                         }
+        .from(nil)
+        .to(completed_review_turnaround.review_request.pull_request_id)
     end
   end
 end
