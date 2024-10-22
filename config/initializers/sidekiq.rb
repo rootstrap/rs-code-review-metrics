292c09a51da0c5d0f4e615558bdaac4773321961
Antonieta Alvarez <53526517+antoalconti@users.noreply.github.com>
2024-10-22 18:12:49 +0200

    Fix redis instance (#586)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,19 @@
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL') { 'redis://localhost:6379/1' }, size: 1, network_timeout: 5 }
+  config.redis = {
+    url: ENV.fetch('REDIS_URL') { 'redis://localhost:6379/1' },
+    size: 1,
+    network_timeout: 5,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL') { 'redis://localhost:6379/1' }, size: 12, network_timeout: 5 }
+  config.redis = {
+    url: ENV.fetch('REDIS_URL') { 'redis://localhost:6379/1' },
+    size: 12,
+    network_timeout: 5,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
 
 schedule_file = 'config/schedule.yml'
