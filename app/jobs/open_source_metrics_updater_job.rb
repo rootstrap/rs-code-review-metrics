50bc5aa58ca121d085a3554d6ae0056d4f464219
gonzaM <gonzalo@rootstrap.com>
2025-04-18 09:11:32 -0300

    Remove unused jobs

--- app/jobs/open_source_metrics_updater_job.rb
+++
@@ -1,7 +0,0 @@
-class OpenSourceMetricsUpdaterJob < ApplicationJob
-  queue_as :default
-
-  def perform
-    Processors::OpenSourceMetricsUpdater.call
-  end
-end
--- app/services/processors/blog_metrics_partial_updater.rb
+++
@@ -1,9 +0,0 @@
-module Processors
-  class BlogMetricsPartialUpdater < BlogMetricsUpdater
-    private
-
-    def technology_views_updater
-      BlogTechnologyViewsPartialUpdater
-    end
-  end
-end
