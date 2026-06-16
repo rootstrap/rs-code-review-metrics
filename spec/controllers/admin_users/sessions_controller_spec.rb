918372a4e02fcf3b5aa96c539ce4db4d9cec5191
German L <german@rootstrap.com>
2026-06-16 14:54:45 +0200

    Fix linter issue

--- spec/controllers/admin_users/sessions_controller_spec.rb
+++ spec/controllers/admin_users/sessions_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AdminUsers::SessionsController, :unauthenticated, type: :controller do
-  before { @request.env['devise.mapping'] = Devise.mappings[:admin_user] }
+  before { request.env['devise.mapping'] = Devise.mappings[:admin_user] }
 
   describe 'GET #new' do
     it 'is reachable without authentication so logged-out users can sign in' do
